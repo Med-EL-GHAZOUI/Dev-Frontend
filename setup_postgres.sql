be46de2deb6b8150da7a28688363141e401a04e7
Med EL-GHAZOUI <elghazoui.md@gmail.com>
2026-05-10 23:21:29 +0100

    BD

---
+++ setup_postgres.sql
@@ -0,0 +1,9 @@
+-- Create the database
+CREATE DATABASE smartsales;
+
+-- Create the user (if you want a specific one, otherwise use postgres)
+-- CREATE USER smartsales_user WITH PASSWORD 'your_password';
+-- GRANT ALL PRIVILEGES ON DATABASE smartsales TO smartsales_user;
+
+-- Verify connection
+\c smartsales;
